a9dc531b11026942d37e55c0e66393f6edaac709
Pasi Sarolahti <pasi.sarolahti@iki.fi>
2016-02-07 23:01:31 +0200

    Added a slow topology.

--- aalto/topologies.py
+++ aalto/topologies.py
@@ -34,5 +34,30 @@ class TopoLatency( Topo ):
         self.addLink( rightSwitch, rightHost1 )
         self.addLink( rightSwitch, rightHost2 )
 
+class TopoSlow( Topo ):
+    "Basic Slow topology for ELEC-E7320."
 
-topos = { 'latency': ( lambda: MyTopo() ) }
+    def __init__( self ):
+        "Create custom topo."
+
+        # Initialize topology
+        Topo.__init__( self )
+
+        # Add hosts and switches
+        leftHost1 = self.addHost( 'lh1' )
+        leftHost2 = self.addHost( 'lh2' )
+        rightHost1 = self.addHost( 'rh1' )
+        rightHost2 = self.addHost( 'rh2' )
+        leftSwitch = self.addSwitch( 's1' )
+        rightSwitch = self.addSwitch( 's2' )
+
+        # Add links
+        self.addLink( leftHost1, leftSwitch )
+        self.addLink( leftHost2, leftSwitch )
+        self.addLink( leftSwitch, rightSwitch, bw=0.05, delay='200ms' )
+        self.addLink( rightSwitch, rightHost1 )
+        self.addLink( rightSwitch, rightHost2 )
+
+topos = { 'latency': ( lambda: TopoLatency() ),
+          'slow': ( lambda: TopoSlow() )
+}
