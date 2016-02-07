4b3d91d5cb9628ebbcf954d00c8e0ae0e7b59244
Pasi Sarolahti <pasi.sarolahti@iki.fi>
2016-02-07 23:30:35 +0200

    Added a couple of more topologies.

--- aalto/topologies.py
+++ aalto/topologies.py
@@ -1,17 +1,18 @@
-"""Custom topology example
+"""ELEC-E7320 Internet Protocols course test topologies
 
-Two directly connected switches plus a host for each switch:
+Modified from mininet custom topology example.
 
-   host --- switch --- switch --- host
+Usage:
+sudo mn -x --custom aalto/topologies.py --link=tc --topo <name>
 
-Adding the 'topos' dict with a key/value pair to generate our newly defined
-topology enables one to pass in '--topo=mytopo' from the command line.
+Where name is 'latency', 'slow', 'buffers', or 'lossy'
 """
+
 
 from mininet.topo import Topo
 
 class TopoLatency( Topo ):
-    "Basic Latency topology for ELEC-E7320."
+    "Long latency"
 
     def __init__( self ):
         "Create custom topo."
@@ -30,12 +31,13 @@ class TopoLatency( Topo ):
         # Add links
         self.addLink( leftHost1, leftSwitch )
         self.addLink( leftHost2, leftSwitch )
-        self.addLink( leftSwitch, rightSwitch, bw=10, delay='200ms' )
+        self.addLink( leftSwitch, rightSwitch, bw=10, delay='400ms' )
         self.addLink( rightSwitch, rightHost1 )
         self.addLink( rightSwitch, rightHost2 )
+
 
 class TopoSlow( Topo ):
-    "Basic Slow topology for ELEC-E7320."
+    "Basic Slow topology small buffers."
 
     def __init__( self ):
         "Create custom topo."
@@ -54,10 +56,63 @@ class TopoSlow( Topo ):
         # Add links
         self.addLink( leftHost1, leftSwitch )
         self.addLink( leftHost2, leftSwitch )
-        self.addLink( leftSwitch, rightSwitch, bw=0.05, delay='200ms' )
+        self.addLink( leftSwitch, rightSwitch, max_queue_size=5, bw=0.1, delay='200ms' )
         self.addLink( rightSwitch, rightHost1 )
         self.addLink( rightSwitch, rightHost2 )
 
+
+class TopoSlowBuffered( Topo ):
+    "Slow overbuffered bottleneck."
+
+    def __init__( self ):
+        "Create custom topo."
+
+        # Initialize topology
+        Topo.__init__( self )
+
+        # Add hosts and switches
+        leftHost1 = self.addHost( 'lh1' )
+        leftHost2 = self.addHost( 'lh2' )
+        rightHost1 = self.addHost( 'rh1' )
+        rightHost2 = self.addHost( 'rh2' )
+        leftSwitch = self.addSwitch( 's1' )
+        rightSwitch = self.addSwitch( 's2' )
+
+        # Add links
+        self.addLink( leftHost1, leftSwitch )
+        self.addLink( leftHost2, leftSwitch )
+        self.addLink( leftSwitch, rightSwitch, max_queue_size=200, bw=0.1, delay='200ms' )
+        self.addLink( rightSwitch, rightHost1 )
+        self.addLink( rightSwitch, rightHost2 )
+
+
+class TopoLossy( Topo ):
+    "10% packet loss rate"
+
+    def __init__( self ):
+        "Create custom topo."
+
+        # Initialize topology
+        Topo.__init__( self )
+
+        # Add hosts and switches
+        leftHost1 = self.addHost( 'lh1' )
+        leftHost2 = self.addHost( 'lh2' )
+        rightHost1 = self.addHost( 'rh1' )
+        rightHost2 = self.addHost( 'rh2' )
+        leftSwitch = self.addSwitch( 's1' )
+        rightSwitch = self.addSwitch( 's2' )
+
+        # Add links
+        self.addLink( leftHost1, leftSwitch )
+        self.addLink( leftHost2, leftSwitch )
+        self.addLink( leftSwitch, rightSwitch, loss=10, bw=10, delay='20ms' )
+        self.addLink( rightSwitch, rightHost1 )
+        self.addLink( rightSwitch, rightHost2 )
+
+
 topos = { 'latency': ( lambda: TopoLatency() ),
-          'slow': ( lambda: TopoSlow() )
+          'slow': (lambda: TopoSlow() ),
+          'buffers': ( lambda: TopoSlowBuffered() ),
+          'lossy': (lambda: TopoLossy() )
 }
